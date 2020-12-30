a8573176c6c711cc5640c6032057e2ac27f4293d
David Denton <denton.david@gmail.com>
2020-12-30 14:44:05 +0000

    work

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
 pub fn concat(a: &str, b: &str) -> String {
     [a, b].join("")
-}
-
-// This is a really bad adding function, its purpose is to fail in this
-// example.
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-    a - b
 }
 
 #[cfg(test)]
@@ -19,23 +8,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }    
-    
-    #[test]
     fn test_concat() {
         let a = "a";
-        let ref_a = &a;
-        let result = concat(ref_a,"b");
-        
-        assert_eq!(result, "ab");
+        let mut ma = a;
+        let result = concat_with_b(ma);
+
+        ma = &result;
+        assert_eq!(ma, "ab");
     }
 
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_ne!(bad_add(1, 2), 3);
+    fn concat_with_b(it: &str) -> String {
+        concat(it, "b")
     }
 }
